0596aa7ef2f23e14f48d09e0edaf9959b868cf68
Martin Jensen <martin.jensen1996@yahoo.dk>
2022-05-18 22:43:33 +0200

    step 2 of signup
    string resources added

---
+++ app/src/main/java/com/example/ticketgoapp/SignUpStep2Fragment.kt
@@ -0,0 +1,31 @@
+package com.example.ticketgoapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.ticketgoapp.databinding.FragmentSignUpStep2Binding
+
+class SignUpStep2Fragment : Fragment() {
+
+    private lateinit var viewModel: SignUpStep2ViewModel
+    private var _binding: FragmentSignUpStep2Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSignUpStep2Binding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+
+        binding.backbtn.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpStep2FragmentBackBtn)
+        }
+
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/ticketgoapp/SignUpStep2ViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.ticketgoapp
+
+import androidx.lifecycle.ViewModel
+
+class SignUpStep2ViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
