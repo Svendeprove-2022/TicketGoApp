8eb3df038fe74ec28d6c03fb1f46a5e38ab19bb6
Martin Jensen <martin.jensen1996@yahoo.dk>
2022-05-18 22:42:32 +0200

    step 1 of signup

---
+++ app/src/main/java/com/example/ticketgoapp/SignUpStep1Fragment.kt
@@ -0,0 +1,31 @@
+package com.example.ticketgoapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.ticketgoapp.databinding.FragmentSignUpStep1Binding
+
+class SignUpStep1Fragment : Fragment() {
+
+    private lateinit var viewModel: SignUpStep1ViewModel
+    private var _binding: FragmentSignUpStep1Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSignUpStep1Binding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+
+        binding.btnContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpStep1Fragment_to_signUpStep2Fragment)
+        }
+
+        return binding.root
+    }
+
+}
