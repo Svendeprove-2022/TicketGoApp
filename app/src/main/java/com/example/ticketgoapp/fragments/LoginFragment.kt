6c4ff37fcd04c152dee6855ceb08ccb276349a76
Martin Jensen <martin.jensen1996@yahoo.dk>
2022-05-19 09:43:04 +0200

    Login view

---
+++ app/src/main/java/com/example/ticketgoapp/fragments/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.example.ticketgoapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.ticketgoapp.databinding.FragmentLoginBinding
+import com.example.ticketgoapp.viewmodels.LoginViewModel
+
+class LoginFragment : Fragment() {
+
+    private lateinit var viewModel: LoginViewModel
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+
+        return binding.root
+    }
+
+}
