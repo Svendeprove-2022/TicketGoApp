90dc7884e01adc4870dd0f3239d6f68fd6de6ed5
Martin Jensen <martin.jensen1996@yahoo.dk>
2022-05-27 12:43:38 +0200

    show users data in profile view with livedata

--- app/src/main/java/com/example/ticketgoapp/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/ticketgoapp/fragments/ProfileFragment.kt
@@ -1,16 +1,18 @@
 package com.example.ticketgoapp.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ticketgoapp.viewmodels.ProfileViewModel
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.ticketgoapp.databinding.FragmentProfileBinding
+import com.example.ticketgoapp.viewmodels.ProfileViewModel
 
 class ProfileFragment : Fragment() {
 
-    private lateinit var viewModel: ProfileViewModel
+    private val viewModel: ProfileViewModel by viewModels()
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
@@ -20,6 +22,12 @@ class ProfileFragment : Fragment() {
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
 
+        viewModel.getUserData().observe(viewLifecycleOwner) {
+            binding.tvHelloUser.text = it.data?.user?.first_name
+            binding.firstnameInput.setText(it.data?.user?.first_name)
+            binding.lastnameInput.setText(it.data?.user?.last_name)
+            binding.emailaddressInput.setText(it.data?.user?.email)
+        }
 
         return binding.root
     }
--- app/src/main/java/com/example/ticketgoapp/viewmodels/ProfileViewModel.kt
+++ app/src/main/java/com/example/ticketgoapp/viewmodels/ProfileViewModel.kt
@@ -1,7 +1,35 @@
 package com.example.ticketgoapp.viewmodels
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.apollographql.apollo3.api.ApolloResponse
+import com.apollographql.apollo3.exception.ApolloException
+import com.example.ticketgoapp.GetDataFromOneUserQuery
+import com.example.ticketgoapp.apollo.apolloClient
+import com.example.ticketgoapp.models.User
+import com.example.ticketgoapp.realm.ticketGoApp
+import kotlinx.coroutines.launch
 
 class ProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private var user: User = User()
+
+    fun getUserData(): LiveData<ApolloResponse<GetDataFromOneUserQuery.Data>> {
+        val response = MutableLiveData<ApolloResponse<GetDataFromOneUserQuery.Data>>()
+
+        viewModelScope.launch {
+            try {
+                response.postValue(
+                    apolloClient().query(GetDataFromOneUserQuery(ticketGoApp.currentUser()!!.id))
+                        .execute()
+                )
+            } catch (e: ApolloException) {
+                Log.d("apollo", "exception: $e")
+            }
+        }
+        return response
+    }
 }
