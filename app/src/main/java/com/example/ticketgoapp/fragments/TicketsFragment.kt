77a0940b2a68c26b5827ddfdd45e0f38f290d7b3
Martin Jensen <martin.jensen1996@yahoo.dk>
2022-05-31 20:26:10 +0200

    and this

--- app/src/main/java/com/example/ticketgoapp/fragments/TicketsFragment.kt
+++ app/src/main/java/com/example/ticketgoapp/fragments/TicketsFragment.kt
@@ -5,12 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.ticketgoapp.viewmodels.TicketsViewModel
 import com.example.ticketgoapp.databinding.FragmentTicketsBinding
 
 class TicketsFragment : Fragment() {
 
-    private lateinit var viewModel: TicketsViewModel
     private var _binding: FragmentTicketsBinding? = null
     private val binding get() = _binding!!
 
@@ -20,8 +18,6 @@ class TicketsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTicketsBinding.inflate(inflater, container, false)
-
-
         return binding.root
     }
 
