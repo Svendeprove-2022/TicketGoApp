26515d212c19504e7e0d8be191a20372f1de80db
Martin Jensen <martin.jensen1996@yahoo.dk>
2022-05-31 20:23:18 +0200

    not used

--- app/src/main/java/com/example/ticketgoapp/viewmodels/HomeViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ticketgoapp.viewmodels
-
-import androidx.lifecycle.ViewModel
-
-class HomeViewModel : ViewModel() {
-
-}
--- app/src/main/java/com/example/ticketgoapp/viewmodels/TicketsViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ticketgoapp.viewmodels
-
-import androidx.lifecycle.ViewModel
-
-class TicketsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
